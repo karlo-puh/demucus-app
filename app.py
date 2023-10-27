f3e0798958b9569701f75878189ebaab8c5fe836
karlo-puh <146028233+karlo-puh@users.noreply.github.com>
2023-10-27 09:48:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 import os
 import streamlit as st
-
-def inference(audio_data):
-    os.makedirs("out", exist_ok=True)
-    
-    # Save the uploaded audio data as a WAV file
-    with open('test.wav', 'wb') as audio_file:
-        audio_file.write(audio_data)
-    
-    # Perform separation here (replace this line with your separation code)
-    os.system("python3 -m demucs.separate -n htdemucs --two-stems=vocals test.wav -o out")
-    
-    return "./out/htdemucs/test/vocals.wav", "./out/htdemucs/test/no_vocals.wav"
+from scipy.io.wavfile import write
+import subprocess
 
 st.title("Demucs Music Source Separation (v4)")
+st.markdown("Music Source Separation in the Waveform Domain | [Github Repo](https://github.com/facebookresearch/demucs) | [//THAFX](https://www.thafx.com)")
 
-uploaded_audio = st.file_uploader("Upload an audio file", type=["wav"])
+audio_input = st.file_uploader("Upload an audio file", type=["wav"])
 
-if uploaded_audio is not None:
-    audio_data = uploaded_audio.read()
-    st.audio(audio_data, format="audio/wav", start_time=0)
-    
-    if st.button("Separate Vocals"):
-        separated_vocals, separated_instrumental = inference(audio_data)
-        st.audio(separated_vocals, format="audio/wav", start_time=0)
-        st.audio(separated_instrumental, format="audio/wav", start_time=0)
+if audio_input is not None:
+    os.makedirs("out", exist_ok=True)
+    with open('test.wav', 'wb') as audio_file:
+        audio_file.write(audio_input.read())
+
+    cmd = "python3 -m demucs.separate -n htdemucs --two-stems=vocals test.wav -o out"
+    subprocess.run(cmd, shell=True)
+
+    vocals_path = "./out/htdemucs/test/vocals.wav"
+    no_vocals_path = "./out/htdemucs/test/no_vocals.wav"
+
+    st.audio(vocals_path, format="audio/wav", label="Vocals")
+    st.audio(no_vocals_path, format="audio/wav", label="No Vocals / Instrumental")
