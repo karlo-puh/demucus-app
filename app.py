e54a95ec5c3e3717e58c0ca8a169ecfa29d56e7c
karlo-puh <146028233+karlo-puh@users.noreply.github.com>
2023-10-27 09:42:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import os
 import streamlit as st
-from scipy.io.wavfile import write
 
-def inference(audio):
+def inference(audio_data):
     os.makedirs("out", exist_ok=True)
-    write('test.wav', audio[0], audio[1])
-    os.system("python3 -m demucs.separate -n htdemucs --two-stems=vocals test.wav -o out")
+    
+    # Save the uploaded audio data as a WAV file
+    with open('test.wav', 'wb') as audio_file:
+        audio_file.write(audio_data)
+    
+    # Perform separation here (replace this line with your separation code)
+    # os.system("python3 -m demucs.separate -n htdemucs --two-stems=vocals test.wav -o out")
+    
     return "./out/htdemucs/test/vocals.wav", "./out/htdemucs/test/no_vocals.wav"
 
 st.title("Demucs Music Source Separation (v4)")
-st.write("""
-<p style='text-align: center'><a href='https://arxiv.org/abs/1911.13254' target='_blank'>Music Source Separation in the Waveform Domain</a> |
-<a href='https://github.com/facebookresearch/demucs' target='_blank'>Github Repo</a> |
-<a href='https://www.thafx.com' target='_blank'>//THAFX</a></p>
-""", unsafe_allow_html=True)
 
 uploaded_audio = st.file_uploader("Upload an audio file", type=["wav"])
 
@@ -22,6 +22,6 @@ if uploaded_audio is not None:
     st.audio(audio_data, format="audio/wav", start_time=0)
     
     if st.button("Separate Vocals"):
-        separated_vocals, separated_instrumental = inference((audio_data, 44100))  # Adjust the sample rate accordingly
+        separated_vocals, separated_instrumental = inference(audio_data)
         st.audio(separated_vocals, format="audio/wav", start_time=0, key="vocals")
         st.audio(separated_instrumental, format="audio/wav", start_time=0, key="instrumental")
